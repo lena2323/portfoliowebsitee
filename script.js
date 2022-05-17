30c1401f293bffc4ccecbd47fa713d97d003c89e
Lena2323 <lena.jovicic23@gmail.com>
2022-05-17 23:40:11 +0200

    h1 animation works, image doesn't.... i broke something again, woohoo!

--- script.js
+++ script.js
@@ -4,3 +4,44 @@ const navUL = document.getElementById('nav-ul');
 hamburger.addEventListener('click',() =>{
     navUL.classList.toggle('show');
 });
+
+
+
+/********* Testing animations oon scroll!**/
+
+const scrollOffset = 100;
+ 
+const scrollElement = document.querySelector(".js-scroll");
+ 
+const elementInView = (el, offset = 0) => {
+  const elementTop = el.getBoundingClientRect().top;
+ 
+  return (
+    elementTop <= 
+    ((window.innerHeight || document.documentElement.clientHeight) - offset)
+  );
+};
+ 
+const displayScrollElement = () => {
+  scrollElement.classList.add('scrolled');
+}
+ 
+const hideScrollElement = () => {
+  scrollElement.classList.remove('scrolled');
+}
+ 
+const handleScrollAnimation = () => {
+  if (elementInView(scrollElement, scrollOffset)) {
+      displayScrollElement();
+  } else {
+    hideScrollElement();
+  }
+}
+
+
+ 
+
+window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+  })
+  /********* Testing animations oon scroll!**/
