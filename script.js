f68c72e44a4122d4ab8628fa88ba75e3a55185e1
Lena2323 <lena.jovicic23@gmail.com>
2022-05-18 00:04:09 +0200

    Animations work now.

--- script.js
+++ script.js
@@ -9,39 +9,46 @@ hamburger.addEventListener('click',() =>{
 
 /********* Testing animations oon scroll!**/
 
-const scrollOffset = 100;
- 
-const scrollElement = document.querySelector(".js-scroll");
- 
-const elementInView = (el, offset = 0) => {
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
   const elementTop = el.getBoundingClientRect().top;
- 
+
   return (
-    elementTop <= 
-    ((window.innerHeight || document.documentElement.clientHeight) - offset)
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
   );
 };
- 
-const displayScrollElement = () => {
-  scrollElement.classList.add('scrolled');
-}
- 
-const hideScrollElement = () => {
-  scrollElement.classList.remove('scrolled');
-}
- 
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
 const handleScrollAnimation = () => {
-  if (elementInView(scrollElement, scrollOffset)) {
-      displayScrollElement();
-  } else {
-    hideScrollElement();
-  }
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
 }
 
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
 
- 
 
-window.addEventListener('scroll', () => {
-    handleScrollAnimation();
-  })
   /********* Testing animations oon scroll!**/
